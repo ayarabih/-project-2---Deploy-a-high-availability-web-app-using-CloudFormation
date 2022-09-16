1634a60802322959b5749281b4864c812ec97b18
Aya Rabih <109382355+ayarabih@users.noreply.github.com>
2022-09-16 19:06:50 +0200

    cloudformatioin

---
+++ network/create.sh
@@ -0,0 +1,1 @@
+ aws cloudformation create-stack --stack-name Udagramserver --template-body file://server-CF.yml --parameters file://serverparamaters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
